01c271b36669a755d12ba006208527fb045bfced
fname lname <monzurulislam997@gmail.com>
2022-11-04 22:31:45 +0600

    feature card desgin

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -4,6 +4,8 @@ import cat2 from '../../assets/image/cat-2.png'
 import cat3 from '../../assets/image/cat-3.png'
 import cat4 from '../../assets/image/cat-4.png'
 import { AiFillCaretRight } from "react-icons/ai";
+import Fade from 'react-reveal/Fade';
+
 const Categories = () => {
     const catagories = [
         {
@@ -32,31 +34,33 @@ const Categories = () => {
                 <h1 className=' text-4xl font-black'>Browse <br />  Popular Categories</h1>
                 <p className='text-slate-500 mt-1'>Find over 7000 website templates and themes.</p>
             </div>
+                <Fade left>
             <div className='grid grid-cols-4 py-5 mx-12'>
 
-                {
+                    {
 
-                    catagories.map(catagorie => <>
+                        catagories.map(catagorie => <>
 
-                        <div className="card w-64 bg-base-100 px-5 hover:border-2 hover:border-indigo-600 drop-shadow-2xl">
-                            <figure className="px-2 pt-10">
-                                <img src={catagorie.img} alt="Shoes" className="rounded-xl" />
-                            </figure>
-                            <div className="card-body card-actions text-center justify-center ">
-                                <h1 className='card-title'>{catagorie.name}</h1>
-                                <button className="flex ml-2     hover:text-indigo-600 hover:font-bold">Explore More
+                            <div className="card w-64 bg-base-100 px-5 hover:border-2 hover:border-indigo-600 drop-shadow-2xl">
+                                <figure className="px-2 pt-10">
+                                    <img src={catagorie.img} alt="Shoes" className="rounded-xl" />
+                                </figure>
+                                <div className="card-body card-actions text-center justify-center ">
+                                    <h1 className='card-title'>{catagorie.name}</h1>
+                                    <button className="flex ml-2     hover:text-indigo-600 hover:font-bold">Explore More
 
-                                    <span className="mt-1  " ><AiFillCaretRight /></span>
-                                </button>
+                                        <span className="mt-1  " ><AiFillCaretRight /></span>
+                                    </button>
 
+                                </div>
                             </div>
-                        </div>
-                    </>)
-                }
+                        </>)
+                    }
             </div>
+                </Fade>
             <div className='text-center'>
-            <button className="py-3 px-8 hover:drop-shadow-2xl rounded-lg my-3 hover:text-cyan-400	 btn-primary">View All Catagories</button>
-          
+                <button className="py-3 px-8 hover:drop-shadow-2xl rounded-lg my-3 hover:text-cyan-400	 btn-primary">View All Catagories</button>
+
             </div>
 
         </div>
--- src/Components/FeatureTemplates/FeatureTemplates.js
+++ src/Components/FeatureTemplates/FeatureTemplates.js
@@ -1,56 +1,57 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Bounce from 'react-reveal/Bounce';
+import { AiOutlineShoppingCart} from "react-icons/ai";
 
 const FeatureTemplates = () => {
+    const [features, setFeatures] = useState([])
+    useEffect(() => {
+        fetch("h.json")
+            .then(res => res.json())
+            .then(data => setFeatures(data))
+    }, [])
+
+
     return (
-        <div>
+        <div >
             <div className='text-center'>
                 <h1 className='text-4xl  font-black '>Fea<span className='text-orange-600'>tu</span>red <br /> Templates  <span className='text-indigo-600'> of The Month</span> </h1>
                 <p className='text-slate-600 mt-1'>All the templates are selected by customer preferences</p>
             </div>
 
-            {/* card  */}
-            <div className='grid w-full grid-cols-3 '>
-                <Fade left>
-
-                    <div className="card w-5/6 bg-base-100 shadow-xl">
-                        <figure><img className='p-3' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Shoes!
-                                <div className="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <div className="badge badge-outline">Fashion</div>
-                                <div className="badge badge-outline">Products</div>
+            <div >
+                {/* card  */}
+            <Bounce >
+                <div className='grid w-full mb-6  lg:grid-cols-3  my-3  ml-8 px-16 '>
+                    {
+                        features.slice(0,6).map(feature => <>
+                            <div className="card w-5/6 bg-base-100 mb-4 drop-shadow-2xl">
+                                <figure><img className='p-3' src={feature.img}alt="Shoes" /></figure>
+                               
+                                <div    className="card-body">
+                                    <h2 className="text-xl font-semibold">
+                                        {feature.name}</h2>
+                                    <p className='mb-1'>By {feature.author}  <span className='bg-slate-200 ml-8 px-2'>{feature.type}</span> </p>
+                                   <div className='flex justify-between'>
+                                    <div className='justify-start'>
+                                    <h1 className='text-xl font-bold'>$ {feature.price}</h1>
+                                    <p>{feature.sale} sales</p>
+                                    </div>
+                                   <div className="card-actions justify-end">
+                                   <p  className='bg-indigo-800 px-5 mt-1 py-1  hover:bg-cyan-600  text-white'>Preview </p>
+                                    <button className='text-2xl px-2 py-1 mt-1  hover:bg-green-600  rounded-sm bg-indigo-600 text-white'> <AiOutlineShoppingCart/></button>
+                                    </div>
+                                   </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="card w-5/6 bg-base-100 shadow-xl">
-                        <figure><img className='p-3' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Shoes!
-                                <div className="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <div className="badge badge-outline">Fashion</div>
-                                <div className="badge badge-outline">Products</div>
-                            </div>
-                        </div>
-                    </div>
-                    
-               
+                        </>)
+                    }
 
 
-
-
-            </Fade>
-
-        </div>
+                </div>
+            </Bounce>
+            </div>
         </div>
     );
 };
